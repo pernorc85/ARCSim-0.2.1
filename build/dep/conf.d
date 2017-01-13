2759ca1cd672addab718d0bb9ef5e6c4d2efc380
Krati Saxena <saxenakrati09@gmail.com>
2017-01-13 19:18:25 +0900

    Add files via upload

---
+++ build/dep/conf.d
@@ -0,0 +1,10 @@
+build/debug/conf.o build/release/conf.o: src/conf.cpp src/conf.hpp src/simulation.hpp src/cloth.hpp \
+ src/dde.hpp src/sparse.hpp src/util.hpp src/mesh.hpp \
+ src/transformation.hpp src/spline.hpp src/vectors.hpp src/winport.hpp \
+ src/constraint.hpp src/handle.hpp src/morph.hpp src/obstacle.hpp \
+ src/timer.hpp src/io.hpp src/magic.hpp src/mot_parser.hpp
+src/conf.cpp src/conf.hpp src/simulation.hpp src/cloth.hpp :
+ src/dde.hpp src/sparse.hpp src/util.hpp src/mesh.hpp :
+ src/transformation.hpp src/spline.hpp src/vectors.hpp src/winport.hpp :
+ src/constraint.hpp src/handle.hpp src/morph.hpp src/obstacle.hpp :
+ src/timer.hpp src/io.hpp src/magic.hpp src/mot_parser.hpp :
---
+++ build/dep/io.d
@@ -0,0 +1,10 @@
+build/debug/io.o build/release/io.o: src/io.cpp src/io.hpp src/mesh.hpp src/transformation.hpp \
+ src/spline.hpp src/vectors.hpp src/winport.hpp src/display.hpp \
+ src/simulation.hpp src/cloth.hpp src/dde.hpp src/sparse.hpp src/util.hpp \
+ src/constraint.hpp src/handle.hpp src/morph.hpp src/obstacle.hpp \
+ src/timer.hpp src/opengl.hpp
+src/io.cpp src/io.hpp src/mesh.hpp src/transformation.hpp :
+ src/spline.hpp src/vectors.hpp src/winport.hpp src/display.hpp :
+ src/simulation.hpp src/cloth.hpp src/dde.hpp src/sparse.hpp src/util.hpp :
+ src/constraint.hpp src/handle.hpp src/morph.hpp src/obstacle.hpp :
+ src/timer.hpp src/opengl.hpp :
